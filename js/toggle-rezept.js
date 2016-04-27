4ba13b401d2209bd810d609d93f3d678805c78d9
Kim Schläpfer <kimschlaepfer@gmx.ch>
2016-04-27 20:19:54 +0200

    scharf/mild in select umgewandelt

--- js/toggle-rezept.js
+++ js/toggle-rezept.js
@@ -11,22 +11,23 @@ $(document).ready(function () {
 
     $('#btn-choice').click(function () {
         var choice = $('#personas').val();
-        if ((choice == 4) && $('#scharf').is(':checked')) {
+        var choice1 = $('#scharfmild').val();
+        if ((choice == 4) && (choice1 == "scharf")) {
             $('#container_4p_mild').hide();
             $('#container_2p_mild').hide();
             $('#container_2p_scharf').hide();
             $('#container_4p_scharf').slideToggle();
-        } else if (choice == 4) {
+        } else if ((choice == 4) && (choice1 == "mild")) {
             $('#container_4p_scharf').hide();
             $('#container_2p_mild').hide();
             $('#container_2p_scharf').hide();
             $('#container_4p_mild').slideToggle();
-        } else if ((choice == "2") && $('#scharf').is(':checked')) {
+        } else if ((choice == "2") && (choice1 == "scharf")) {
             $('#container_4p_mild').hide();
             $('#container_4p_scharf').hide();
             $('#container_2p_mild').hide();
             $('#container_2p_scharf').slideToggle();
-        } else if (choice == "2") {
+        } else if ((choice == "2") && (choice1 == "mild")) {
             $('#container_4p_mild').hide();
             $('#container_4p_scharf').hide();
             $('#container_2p_scharf').hide();
